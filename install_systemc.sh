be682815e56986dbbe353cca91d78cd5abe624da
Éder F. Zulian <zulian@eit.uni-kl.de>
2017-11-08 16:19:25 +0100

    enhancement

--- install_systemc.sh
+++ install_systemc.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/sh
+#!/usr/bin/env sh
 #Install systemc on a ubuntu like linux
 wget http://www.accellera.org/images/downloads/standards/systemc/systemc-2.3.1a.tar.gz
 tar xfv systemc-2.3.1a.tar.gz
 cd systemc-2.3.1a
 ./configure --prefix=/opt/systemc/
-make -j 4
+make -j4
 sudo make install
