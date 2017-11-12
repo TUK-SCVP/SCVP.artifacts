e400cdb67e25102975a418097bb769074a90bffd
Matthias Jung <jungma@eit.uni-kl.de>
2017-11-12 18:55:35 +0100

    clock generator example added

---
+++ clock_generator/clock_generator.pro
@@ -0,0 +1,5 @@
+TARGET = clock_generator
+
+include(../common.pri)
+
+SOURCES += main.cpp
---
+++ clock_generator/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <systemc.h>
+
+using namespace std;
+
+SC_MODULE(clockGenerator)
+{
+    public:
+    sc_out<bool> clk;
+    bool value;
+    sc_time period;
+
+    SC_HAS_PROCESS(clockGenerator);
+    clockGenerator(const sc_module_name &name, sc_time period) :
+       sc_module(name),
+       period(period),
+       value(true)
+    {
+        SC_THREAD(generation);
+    }
+    private:
+    void generation()
+    {
+        while(true)
+        {
+            value = !value;
+            clk.write(value);
+            wait(period/2);
+        }
+    }
+};
+
+
+int sc_main (int __attribute__((unused)) sc_argc,
+             char __attribute__((unused)) *sc_argv[])
+{
+    clockGenerator g("clock_1GHz", sc_time(1,SC_NS));
+    sc_signal<bool> clk;
+
+    // Bind Signals
+    g.clk.bind(clk);
+
+    // Setup Waveform Tracing:
+    sc_trace_file *wf = sc_create_vcd_trace_file("traceFile");
+    sc_trace(wf, clk, "clk");
+
+    // Start Simulation
+    sc_start(10, SC_NS);
+
+    // Close Trace File:
+    sc_close_vcd_trace_file(wf);
+
+    return 0;
+}
